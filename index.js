2cedcb8ce1ffa3a28a54c9c9fc4996dd0c96d125
John Kudela <Nhoj1002@gmail.com>
2026-01-07 17:17:46 -0500

    ensure generated dir created first

--- index.js
+++ index.js
@@ -9,51 +9,43 @@ module.exports = ({srcDirectory, path, modulo = 3}) => {
     const destinationDirectory = path;
 
     // Ensure generated dir exists
-    exec(`mkdir ${generatedFFDirectory}`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error creating Generated Feature dir: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return;
-        }
-        console.log('Generated feature dir');
-    });
+    exec(`mkdir ${generatedFFDirectory}`, () => {
+        console.log('Attempted to generate feature dir');
 
-    // Do the sliceage
-    const featureFiles = glob.sync(`.${path}/**/*.feature`);
-    cucumberSlicer(featureFiles, generatedFFDirectory, modulo);
+        // Do the sliceage
+        const featureFiles = glob.sync(`.${path}/**/*.feature`);
+        cucumberSlicer(featureFiles, generatedFFDirectory, modulo);
 
-    // Bash command to delete *.feature files recursively from the destination directory
-    const deleteCommand = `find ${destinationDirectory} -type f -name "*.feature" -delete`;
+        // Bash command to delete *.feature files recursively from the destination directory
+        const deleteCommand = `find ${destinationDirectory} -type f -name "*.feature" -delete`;
 
-    // Bash command to move files/folders from source to destination directory
-    const moveCommand = `mv ${generatedFFDirectory}/* ${destinationDirectory}`;
+        // Bash command to move files/folders from source to destination directory
+        const moveCommand = `mv ${generatedFFDirectory}/* ${destinationDirectory}`;
 
-    // Execute the delete command
-    exec(deleteCommand, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error deleting .feature files: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.error(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`Deleted .feature files from ${destinationDirectory}`);
-
-        // Execute the move command
-        exec(moveCommand, (error, stdout, stderr) => {
+        // Execute the delete command
+        exec(deleteCommand, (error, stdout, stderr) => {
             if (error) {
-                console.error(`Error moving files: ${error.message}`);
+                console.error(`Error deleting .feature files: ${error.message}`);
                 return;
             }
             if (stderr) {
                 console.error(`stderr: ${stderr}`);
                 return;
             }
-            console.log(`Moved files from ${generatedFFDirectory} to ${destinationDirectory}`);
+            console.log(`Deleted .feature files from ${destinationDirectory}`);
+
+            // Execute the move command
+            exec(moveCommand, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error moving files: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.error(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`Moved files from ${generatedFFDirectory} to ${destinationDirectory}`);
+            });
         });
     });
 };
